128a9b960bff0647fa22ed3f9c89d775f692d6a8
adamdepollo <adamdepollo@gmail.com>
2019-10-31 10:29:23 -0400

    initial commit

---
+++ src/co/gc/MainApp.java
@@ -0,0 +1,11 @@
+package co.gc;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+		System.out.println("Hello, my name is Antonella.");
+
+	}
+
+}
