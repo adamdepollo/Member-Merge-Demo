cc8a6649f577c009d4579175e77872b4b0c4a1ff
adamdepollo <adamdepollo@gmail.com>
2019-10-31 10:51:29 -0400

    second commit Adam

--- src/co/gc/MainApp.java
+++ src/co/gc/MainApp.java
@@ -4,7 +4,7 @@ public class MainApp {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
-		System.out.println("Hello, my name is Antonella.");
+		System.out.println("Hello, my name is Adam.");
 		System.out.println("Happy Halloween!");
 
 	}
