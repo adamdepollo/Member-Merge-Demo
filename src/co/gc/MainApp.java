78e8dbb750a58f2bf59b7f5b50d5603ae1863472
Kevin Chung <chungkev7@gmail.com>
2019-10-31 10:46:48 -0400

    added a line

--- src/co/gc/MainApp.java
+++ src/co/gc/MainApp.java
@@ -5,6 +5,7 @@ public class MainApp {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
 		System.out.println("Hello, my name is Antonella.");
+		System.out.println("Happy Halloween!");
 
 	}
 
