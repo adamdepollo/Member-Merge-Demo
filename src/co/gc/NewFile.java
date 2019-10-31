91d6a58d7d581c4aaff99ba02fa69e441b12b9ec
adamdepollo <adamdepollo@gmail.com>
2019-10-31 10:59:29 -0400

    new class added

---
+++ src/co/gc/NewFile.java
@@ -0,0 +1,17 @@
+package co.gc;
+
+public class NewFile {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
